ea31244d9efbe04da756022eaf5f1140b3b3986a
Marko <marko.tunjic@fer.hr>
2022-03-29 17:28:09 +0200

    add initial backend files

---
+++ backend/zavrsni_rad/src/test/java/hr/fer/zpr/marko_tunjic/zavrsni_rad/ZavrsniRadApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.zpr.marko_tunjic.zavrsni_rad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniRadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
