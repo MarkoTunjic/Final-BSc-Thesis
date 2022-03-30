9979c9ddbc078afdf70d56fa855269a78c2ce8ee
Marko <marko.tunjic@fer.hr>
2022-03-30 23:37:41 +0200

    remove default spring security page

--- backend/zavrsni_rad/src/main/java/hr/fer/zpr/marko_tunjic/zavrsni_rad/ZavrsniRadApplication.java
+++ backend/zavrsni_rad/src/main/java/hr/fer/zpr/marko_tunjic/zavrsni_rad/ZavrsniRadApplication.java
@@ -2,8 +2,9 @@ package hr.fer.zpr.marko_tunjic.zavrsni_rad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ZavrsniRadApplication {
 
 	public static void main(String[] args) {
